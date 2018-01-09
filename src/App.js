8e2f824f4413a204b974fb14bb90cd5772196ce2
David Aguilar <davidaguilarh.94@gmail.com>
2018-01-08 19:25:08 -0500

    Add task with submit button

--- src/App.js
+++ src/App.js
@@ -3,22 +3,46 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.updateInput = this.updateInput.bind(this)
+    this.addTask = this.addTask.bind(this)
+    this.state = {
+      newTask: '',
+      tasks: ['Sacar la ropa', 'Hacer la cama', 'Leer un rato']
+    }
+  }
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.tasks.map((task, i) =>
+              <li key={i}>{task}</li>
+            )}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={this.addTask}>
+             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} onChange={this.updateInput} />
+             <input type='submit' vlaue='submit' />
            </form>
         </div>
       </div>
     )
+  }
+  updateInput(e) {
+    this.setState({
+      newTask: e.target.value
+    })
+  }
+
+  addTask(e) {
+      e.preventDefault();
+      this.setState({
+        tasks: [...this.state.tasks, this.state.newTask],
+        newTask: ''
+      })
   }
 }
 
